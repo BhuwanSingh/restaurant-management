c7a6984bc7b763309886c4527ed7e1dd63c6c324
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2025-06-27 10:24:08 +0530

    routes in main set

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"os"
+	// "github.com/BhuwanSingh/restaurant-management/database"
+	// "github.com/BhuwanSingh/restaurant-management/routes"
+	// "github.com/BhuwanSingh/restaurant-management/middleware"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
 
+var foodCollection *mongo.Collection = database.openCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.Run(":" + port)
 }
---
+++ routes/userRouter.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	// "github.com/BhuwanSingh/restaurant-management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controller.GetUsers())
+}
